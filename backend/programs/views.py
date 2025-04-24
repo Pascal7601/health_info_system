bfc8b5403be5e46a60f18cd45f3bcd4053c9be8f
Pascal7601 <levispascal63@gmail.com>
2025-04-25 00:48:26 +0300

    add backend apis for program, doctor, clients

--- backend/programs/views.py
+++ backend/programs/views.py
@@ -35,6 +35,17 @@ def create_program(
   db.refresh(pg)
   return {"message": "program added succesfully"}
 
+@program_router.get('/programs')
+def get_all_programs(
+  db: Session = Depends(get_db),
+  credentials: HTTPBasicCredentials = Depends(security)
+):
+  """
+  get all health programs in the system
+  """
+  programs = db.query(Program).all()
+  return programs
+
 @program_router.post('/program/{program_id}/{client_id}')
 def add_client_in_program(
   program_id: str, client_id: str, db: Session = Depends(get_db),
@@ -50,3 +61,39 @@ def add_client_in_program(
     raise HTTPError.not_found('program does not exist')
   
   client = db.query(Client).filter(Client.id == client_id).first()
+  if not client:
+    raise HTTPError("client is not found in the system")
+  
+  enroll = Enrollment(
+    client_id=client.id,
+    program_id=program.id
+  )
+  db.add(enroll)
+  db.commit()
+  db.refresh(enroll)
+  return {"message": f"client added succesfully to {program.name} program"}
+
+@program_router.get('/{program_id}/clients')
+def get_clients_from_program(
+  program_id: str, db: Session = Depends(get_db),
+  credentials: HTTPBasicCredentials = Depends(security)
+):
+  """
+  get clients from a certain program
+  """
+  payload = AuthHandler.decode_token(credentials.credentials) # obtain the token from the header
+  doctor_email = payload.get('sub') # obtain docto's email
+  if not doctor_email:
+    raise HTTPError.unauthorized('cannot create program') # must be authenticated to create a program
+  
+  program = db.query(Program).filter(Program.id == program_id).first()
+  if not program:
+    raise HTTPError.not_found('program does not exist')
+  
+  # Get all enrollment records for this program
+  enrollments = db.query(Enrollment).filter(Enrollment.program_id == program_id).all()
+
+  client_ids = [enroll.client_id for enroll in enrollments] # Get all clients associated with these enrollments
+  clients = db.query(Client).filter(Client.id.in_(client_ids)).all()
+
+  return clients
